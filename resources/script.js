cb94e50d8a21add2b1d26cab269e9a6c2505641d
Diana <dianagithubdavis@gmail.com>
2022-07-04 07:35:13 -0400

    Adds rules with alterations for special cases

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,47 @@
-const fancyName = (firstName) => {
-    let userInput = document.getElementById('firstName');
-    let inputFirstName = userInput.value; 
-    console.log(inputFirstName);
+//Basic rules work
+//Need to clear form after user input
+//Need to display result for user; right now it's just in console
+//Need to check input for sneaky words that would turn into swear words
+
+
+const fancyName = () => {
+    //get first name from fprm
+    const userInput = document.getElementById('firstName');
+    const inputFirstName = userInput.value;
+    //capitalize first letter, lowercase the rest even if user didn't
+    const firstNameFixCaps = inputFirstName.charAt(0).toUpperCase() + inputFirstName.slice(1).toLowerCase();
+    //will alter it later; use let instead of const
+    let alteredFirstName;
+    //these may change depending on first charactter of name; use let instead of const
+    let boB = 'bo-b';
+    let foF = 'fo-f';
+    let moM = 'mo-m';
+    const vowels = 'AEIOUY';
+    //alias for first character
+    const firstChar = firstNameFixCaps[0];
+    //if name starts with a vowel
+    if (vowels.includes(firstChar)) {
+        //keep first letter but lowercase it
+        alteredFirstName = firstNameFixCaps.toLowerCase();
+    } else {
+        //use name as is
+        alteredFirstName = firstNameFixCaps.slice(1)
+    }
+    //check for letters with special rules
+    if (firstChar === 'B') {
+        //alter this part to remove leading B
+        boB = 'bo-';
+    } else if (firstChar === 'F') {
+        //alter this part to remove leading F
+        foF = 'fo-';
+    } else if (firstChar === 'M') {
+        //alter this part to remove leading M
+        moM = 'mo-';
+    }  
+
+    const nameGame = `${firstNameFixCaps}, ${firstNameFixCaps}, ${boB}${alteredFirstName}, 
+    Banana-fana ${foF}${alteredFirstName}, 
+    Fee-fi-${moM}${alteredFirstName},
+    ${firstNameFixCaps}!`;
+    console.log(nameGame)
 }
